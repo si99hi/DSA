8edc4b0b97d37adfd1487c9a0a429b337795e18d
siddhi <siddhib011@gmail.com>
2026-06-27 22:20:39 +0530

    Time: 5 ms (16.98%), Space: 23.7 MB (11.59%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -3,17 +3,18 @@ public:
     string reverseWords(string s) {
         string words;
         stringstream ss(s);
-        string result= "";
-
+        string result="";
         while(ss>> words){
             if(result==""){
                 result= words;
             }
             else{
-                result= words+ " " + result;
+                result=  words + " " + result;
             }
+
         }
         return result;
+
         
     }
 };
