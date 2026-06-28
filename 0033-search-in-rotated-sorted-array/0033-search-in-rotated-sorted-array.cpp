e1045e12a0c33331d84e1a36a504e2e9d23b4ba1
siddhi <siddhib011@gmail.com>
2026-06-28 16:48:33 +0530

    Time: 0 ms (100%), Space: 15.1 MB (67.33%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -4,33 +4,35 @@ public:
         int left=0;
         int right= nums.size()-1;
         while(left<=right){
-            int mid= left + (right-left)/2;
+            int mid= left+ (right-left)/2;
 
             if(nums[mid]== target){
                 return mid;
             }
 
             if(nums[left] <= nums[mid]){
-                if(target>=nums[left]  && target<nums[mid]){
+                if(nums[left] <= target && target< nums[mid]){
                     right= mid-1;
                 }
                 else{
                     left= mid+1;
                 }
+                
             }
-            else{
-
-                if(target>nums[mid] && target<= nums[right]){
+            else {
+                if(target> nums[mid] && target<=nums[right]){
                     left= mid+1;
-
                 }
                 else{
                     right= mid-1;
                 }
-            }
 
+
+                
+            }
+            
+            
         }
-
         return -1;
         
     }
