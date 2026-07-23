418300df27708a7884a5c279cba1e00728d75acb
siddhi <siddhib011@gmail.com>
2026-07-23 14:50:13 +0530

    Time: 12 ms (42.84%), Space: 11.9 MB (54.47%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -11,18 +11,15 @@ public:
     bool hasCycle(ListNode *head) {
         ListNode* slow= head;
         ListNode* fast= head;
-        if(head== nullptr || head->next== nullptr){
-            return false;
-        }
         while(fast!= nullptr  && fast->next!= nullptr){
+
             slow= slow->next;
             fast= fast->next->next;
-
             if(slow==fast){
                 return true;
             }
         }
-
         return false;
+        
     }
 };
