886a42ba7f50b3a7177d997571f58a3061a0eb0d
siddhi <siddhib011@gmail.com>
2026-08-04 16:32:12 +0530

    Time: 0 ms (100%), Space: 9.4 MB (77.05%) - LeetHub

--- 0059-spiral-matrix-ii/0059-spiral-matrix-ii.cpp
+++ 0059-spiral-matrix-ii/0059-spiral-matrix-ii.cpp
@@ -7,25 +7,24 @@ public:
         int top=0;
         int bottom= n-1;
         int nums=1;
-
-        while(left<=right && top<=bottom){
+        while(left<=right  && top<=bottom){
             for(int i=left; i<=right; i++){
-                ans[top][i]= nums++;
+                ans[top][i]=nums++;
             }
             top++;
 
             for(int i=top; i<=bottom; i++){
-                ans[i][right]=nums++;
-            }
-            right--;
+                
+                    ans[i][right]= nums++;
+                }
+                right--;
+            
 
-            if(left<=right){
+            if(top<=bottom){
                 for(int i=right; i>=left; i--){
-                    ans[bottom][i]= nums++;
+                    ans[bottom][i]=nums++;
                 }
                 bottom--;
-
-
             }
 
             if(top<=bottom){
@@ -36,7 +35,6 @@ public:
             }
         }
         return ans;
-
         
     }
 };
