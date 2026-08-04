be9c30272299b85c857ba5e2efd75d90b6166f31
siddhi <siddhib011@gmail.com>
2026-08-04 08:57:51 +0530

    Time: 6 ms (6.63%), Space: 42.3 MB (17.66%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -6,13 +6,13 @@ public:
         vector<int> suffix(n,1);
         vector<int> res(n);
         for(int i=1; i<n; i++){
-            prefix[i]= prefix[i-1] * nums[i-1];
+            prefix[i]= prefix[i-1]* nums[i-1];
         }
         for(int i=n-2; i>=0; i--){
             suffix[i]= suffix[i+1] * nums[i+1];
         }
-        for(int i=0; i< n; i++){
-            res[i]= prefix[i]* suffix[i];
+        for(int i=0; i<n; i++){
+            res[i]= prefix[i] * suffix[i];
         }
         return res;
         
