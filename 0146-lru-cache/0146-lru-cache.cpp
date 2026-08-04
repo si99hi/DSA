24d7f90c422d40a24fc682ce831b28696cf64cdc
siddhi <siddhib011@gmail.com>
2026-08-04 21:32:44 +0530

    Time: 114 ms (17.95%), Space: 184.1 MB (20.18%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.cpp
+++ 0146-lru-cache/0146-lru-cache.cpp
@@ -1,47 +1,49 @@
 class LRUCache {
 public:
-
-    unordered_map<int, int> map;
+    unordered_map<int, int>map;
     unordered_map<int , list<int>::iterator>pos;
-    list<int> lru;
-
+    list<int>lru;
     int cap;
 
+    
     LRUCache(int capacity) {
-         cap= capacity;
+        cap= capacity;
         
     }
     
     int get(int key) {
+
+        //if you dont get key at end
         if(map.find(key)==map.end()){
             return -1;
         }
-
         lru.erase(pos[key]);
         lru.push_front(key);
-        pos[key]= lru.begin();    
+        pos[key]=lru.begin();
 
-        return map[key];    
+        return map[key];
+        
     }
     
     void put(int key, int value) {
 
-        if(map.find(key)!= map.end()){
+        if(map.find(key)!=map.end()){
             lru.erase(pos[key]);
-        }
-        else if(map.size()==cap){
+
+        } 
+        else if(map.size()== cap){
             int oldKey= lru.back();
             lru.pop_back();
             map.erase(oldKey);
             pos.erase(oldKey);
 
         }
+
         lru.push_front(key);
-        pos[key]=lru.begin();
+        pos[key]= lru.begin();
         map[key]=value;
         
     }
-
 };
 
 /**
