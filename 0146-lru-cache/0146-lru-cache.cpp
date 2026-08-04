0a6923acc7902bd5efe246ff10fa3460ed69ab89
siddhi <siddhib011@gmail.com>
2026-08-04 21:21:12 +0530

    Time: 101 ms (30.73%), Space: 184 MB (20.55%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.cpp
+++ 0146-lru-cache/0146-lru-cache.cpp
@@ -1,40 +1,52 @@
 class LRUCache {
 public:
-    unordered_map<int, int> mp; 
-    unordered_map<int, list<int>::iterator> pos; 
-    list<int> lru; 
+
+    unordered_map<int, int> map;
+    unordered_map<int , list<int>::iterator>pos;
+    list<int> lru;
+
     int cap;
 
     LRUCache(int capacity) {
-        cap = capacity;
+         cap= capacity;
+        
     }
-
+    
     int get(int key) {
-
-        if (mp.find(key) == mp.end())
+        if(map.find(key)==map.end()){
             return -1;
+        }
 
-        lru.erase(pos[key]);      
-        lru.push_front(key);      
-        pos[key] = lru.begin();
+        lru.erase(pos[key]);
+        lru.push_front(key);
+        pos[key]= lru.begin();    
 
-        return mp[key];
+        return map[key];    
     }
-
+    
     void put(int key, int value) {
 
-        if (mp.find(key) != mp.end()) {
+        if(map.find(key)!= map.end()){
             lru.erase(pos[key]);
         }
-        else if (mp.size() == cap) {
-            int oldKey = lru.back();
+        else if(map.size()==cap){
+            int oldKey= lru.back();
             lru.pop_back();
-            mp.erase(oldKey);
+            map.erase(oldKey);
             pos.erase(oldKey);
-        }
 
+        }
         lru.push_front(key);
-        pos[key] = lru.begin();
-        mp[key] = value;
+        pos[key]=lru.begin();
+        map[key]=value;
+        
     }
+
 };
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache* obj = new LRUCache(capacity);
+ * int param_1 = obj->get(key);
+ * obj->put(key,value);
+ */
